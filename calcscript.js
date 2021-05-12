19e9a39798737f70c4661fccae757501f89d0b4d
kesia1998 <kesiascia1998@gmail.com>
2021-05-12 10:28:27 +0530

    calculator

---
+++ calcscript.js
@@ -0,0 +1,11 @@
+function displayBox(num){
+    inpt.value+=num;
+}
+function evaluateExpression(){
+    let val=inpt.value
+    let result=eval(val)
+    inpt.value=result
+}
+function clearBox(){
+    inpt.value=""
+}
